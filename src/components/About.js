5876ced43421682067bf249ea231d0cf7b4b4ded
Jovan Babic <jbabic999@gmail.com>
2021-09-29 15:06:00 +0200

    About me picture changed

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ export default function About() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img src="./coding.svg" alt="" className="object-cover object-center rounded" />
+          <img src="./profile.png" alt="" className="object-cover object-center rounded" />
         </div>
       </div>
     </section>
